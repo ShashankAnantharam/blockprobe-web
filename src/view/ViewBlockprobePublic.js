eb3de6c049c6f591d962108b20305e8d45baad21
Shashank <shashank95.a@gmail.com>
2019-03-05 04:22:25 +0530

    started

--- src/view/ViewBlockprobePublic.js
+++ src/view/ViewBlockprobePublic.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+import { timingSafeEqual } from 'crypto';
 
 // /view/3a30893249f6952e26de1ce709094e6952731beb9e37c244c07e542e81f52227
 
@@ -10,37 +11,39 @@ class ViewBlockprobePublicComponent extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            testList:[]
+            blockTree: {}
         }
     }
 
-    addBlocksToProbe(doc){
-        this.setState((prevState) => ({
-            testList: [...prevState.testList, ...doc.data().blocks]
-         }));
+    addBlocksToProbe(doc){      
+         doc.data().blocks.forEach(block => {
+             var tempState = this.state.blockTree;
+             tempState[block.key] = block;
+             this.setState({
+                 blockTree:tempState
+             })
+         });
     }
 
     createBlockprobe(snapshot){
-
         snapshot.forEach((doc) => ( this.addBlocksToProbe(doc)));
-
     }
 
     componentDidMount(){
-
         firebase.firestore().collection("public").doc(this.props.match.params.bId)
         .collection("aggBlocks").get().then((snapshot) => (
-
             this.createBlockprobe(snapshot)
          ));
-
     }
 
     render(){
+        const mapItems = Object.keys(this.state.blockTree).map((key, index) => 
+        <li>{key}</li>);
         return (
             <div>
           <div>{this.props.match.params.bId}</div>
-            <div>{this.state.testList.length}</div>
+            <div>{Object.keys(this.state.blockTree).length}</div>           
+            <div>{mapItems}</div>
             </div>
             );
     }
