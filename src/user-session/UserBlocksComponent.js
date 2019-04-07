b0c02af45a88717409ceef7552be51fd74ec8cdf
Shashank <shashank95.a@gmail.com>
2019-04-07 11:01:02 +0530

    Added stuff

--- src/user-session/UserBlocksComponent.js
+++ src/user-session/UserBlocksComponent.js
@@ -11,15 +11,125 @@ class UserBlocksComponent extends React.Component {
 
         this.state={
             uIdHash:'',
-            shajs:null
+            shajs:null,
+            draftBlocks:{},
+            successBlocks:{},
+            toReviewBlocks:{},
+            inReviewBlocks:{},
+            blockStateMap:{}
         }
         //props include bpId, uId
         var shajs = require('sha.js');
         this.state.uIdHash = shajs('sha256').update(this.props.uId).digest('hex');
         this.state.shajs = shajs;
 
+        this.modifyBlockList = this.modifyBlockList.bind(this);
+        this.modifyBlockListWrapper = this.modifyBlockListWrapper.bind(this);
+
     }
 
+
+    modifyBlockList(block, add){
+        if(block.blockState=="SUCCESSFUL"){
+            var currMap = this.state.successBlocks;
+            if(add)
+                currMap[block.key]=block;
+            else
+                delete currMap[block.key];
+            this.setState({
+                successBlocks:currMap
+            });
+        }
+        else if(block.blockState=="UNDER REVIEW"){
+            var currMap = this.state.inReviewBlocks;
+            if(add)
+                currMap[block.key]=block;
+            else
+                delete currMap[block.key];
+            this.setState({
+                inReviewBlocks:currMap
+            });
+            console.log(block.blockState);
+            console.log(block);
+            console.log(add);
+        }
+        else if(block.blockState=="TO REVIEW"){
+            var currMap = this.state.toReviewBlocks;
+            if(add)
+                currMap[block.key]=block;
+            else
+                delete currMap[block.key];
+            this.setState({
+                toReviewBlocks:currMap
+            });
+        }
+        else if(block.blockState=="DRAFT"){
+            var currMap = this.state.draftBlocks;
+            if(add)
+                currMap[block.key]=block;
+            else
+                delete currMap[block.key];
+            this.setState({
+                draftBlocks:currMap
+            });
+        }
+    }
+
+
+    modifyBlockListWrapper(doc, add){
+        var block = doc.data();
+        var blockId = doc.id;
+        var blockStateMap = this.state.blockStateMap;
+        if(blockId in blockStateMap){
+            var prevState = blockStateMap[blockId];
+            var oldBlock = {
+                key:block.key,
+                blockState: prevState
+            };
+            //delete old block
+            this.modifyBlockList(oldBlock,false);
+
+            if(add){
+                //add new block
+                this.modifyBlockList(block,true);
+
+                //Update blockstate
+               blockStateMap[blockId] = block.blockState;
+            }
+        }
+        else if(add){
+            //First time block gets added
+            blockStateMap[blockId] = block.blockState;
+            this.modifyBlockList(block,true);
+        }
+    }
+
+    componentDidMount(){
+        firebase.firestore().collection("Blockprobes").doc(this.props.bId)
+        .collection("users").doc(this.state.uIdHash).collection("userBlocks").onSnapshot(
+            querySnapshot => {
+                querySnapshot.docChanges().forEach(change => {
+                    if (change.type === 'added') {
+                        //console.log('New block: ', change.doc.data());
+                        this.modifyBlockListWrapper(change.doc,true);
+                      }
+                      if (change.type === 'modified') {
+                        //console.log('Modified block: ', change.doc.data());
+                        this.modifyBlockListWrapper(change.doc,true);
+                      }
+                      if (change.type === 'removed') {
+                        //console.log('Removed block: ', change.doc.data());
+                        this.modifyBlockListWrapper(change.doc,false);
+                      }
+                });
+
+                console.log(this.state.successBlocks);
+                console.log(this.state.inReviewBlocks);
+                console.log(this.state.toReviewBlocks);
+                console.log(this.state.draftBlocks);
+            }
+        );    
+    }
 
     render(){
         return(
