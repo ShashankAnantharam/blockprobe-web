74fc6f81c232f7047976e8a3bc2598fac748e254
Shashank <shashank95.a@gmail.com>
2019-08-31 16:26:50 +0530

    dd

--- src/view/ImagePane/ImagePane.js
+++ src/view/ImagePane/ImagePane.js
@@ -38,7 +38,6 @@ class ImagePaneView extends React.Component {
             doc(this.props.bId).
             collection("images").
             doc(newImage.entity).set(newImage);
-
         }
 
     }
