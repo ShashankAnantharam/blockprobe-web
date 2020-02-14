d1e05d4b6b7bdaf74654a181a261c4e7ccb86361
Shashank <shashank95.a@gmail.com>
2020-02-15 02:01:08 +0530

    timeline sound added

--- src/viso/TimelineComponent.js
+++ src/viso/TimelineComponent.js
@@ -3,18 +3,156 @@ import './TimelineComponent.css';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import Img from 'react-image';
 import ReactGA from 'react-ga';
+import PlayArrow from '@material-ui/icons/PlayArrow';
+import Pause from '@material-ui/icons/Pause';
+import Stop from '@material-ui/icons/Stop';
+import Speech from 'speak-tts';
 import  * as  Utils from '../common/utilSvc';
 import 'react-vertical-timeline-component/style.min.css';
 import { isNullOrUndefined } from 'util';
+
+const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+const isIE = /*@cc_on!@*/false || !!document.documentMode;
 
 class TimelineComponent extends React.Component {
 
     constructor(props){
       super(props);
+
+      this.state = {
+          playStatus: 'end',
+          isSpeechAvailable: false
+      }
+
+      this.speech = null;
       
       this.selectTimelineBlock = this.selectTimelineBlock.bind(this);
       this.removeHashedIndex = this.removeHashedIndex.bind(this);
+
+    this.initSpeech = this.initSpeech.bind(this);
+    this.playExistingSelection = this.playExistingSelection.bind(this);
+    this.stopExistingSelection = this.stopExistingSelection.bind(this);
+    this.timeoutFn = this.timeoutFn.bind(this);
+    this.timeInFn = this.timeInFn.bind(this);
+
       ReactGA.initialize('UA-143383035-1');  
+    }
+
+    async timeoutFn(){  
+        if(this.state.playStatus == 'start' && !isNullOrUndefined(this.speech) && isChrome){
+            await this.speech.pause();
+            await this.speech.resume();     
+        }     
+    }
+
+    timeInFn(){
+        const scope = this;
+        this.timeout = setInterval(() => {            
+            this.timeoutFn();                
+          }, 8500);
+    }
+
+    async initSpeech(){
+        try{
+            this.speech = new Speech();
+            if(this.speech.hasBrowserSupport()) { // returns a boolean
+                // console.log("speech synthesis supported")
+            }
+            let data = await this.speech.init();  
+            
+            if(!isNullOrUndefined(data)){
+                this.setState({
+                    isSpeechAvailable: true
+                });
+            }
+            
+            let voices =  data.voices;
+            let selectedVoice = -1;
+            for(let i=0; !isNullOrUndefined(voices) && i<voices.length; i++){
+                if(selectedVoice == -1)
+                    selectedVoice = i;
+                // firebase.database().ref('Testing/dataVal/'+String(i)).set(voices[i].name);
+                let name = voices[i].name;
+                if(name.toLowerCase().includes('eng') || name.toLowerCase().includes('catherine')) 
+                {
+                    selectedVoice = i;
+                    break;
+                }
+            }
+            if(selectedVoice != -1){
+                //firebase.database().ref('Testing/dataVal').set(voices[selectedVoice].name);
+                await this.speech.setVoice(voices[selectedVoice].name);
+            }
+        }
+        catch{}
+    }
+
+    async stopExistingSelection(){
+        if(!isNullOrUndefined(this.speech)){
+            await this.speech.cancel();
+            this.setState({
+                playStatus: 'end'
+            });    
+        }
+    }
+
+    async playExistingSelection(){
+        if(!isNullOrUndefined(this.speech)){
+            if(this.speech.speaking())
+                await this.speech.cancel();
+          
+            let selectedNodesString = 'timeline';
+            ReactGA.event({
+                category: 'playSound',
+                action: 'PlaySound ' + selectedNodesString,
+                label: 'PlaySound ' + selectedNodesString
+            });
+
+            let toPlayText = '';
+            this.props.timeline.map((selectedBlock) => 
+                {
+                    let dateTimeStr = Utils.getDateTimeString(selectedBlock);
+                    let title = this.removeHashedIndex(selectedBlock.title);
+                    let summary = selectedBlock.summary;
+
+                    if(!isNullOrUndefined(dateTimeStr) && dateTimeStr.length>0){
+                        toPlayText += dateTimeStr + ". ";
+                    }
+                    if(!isNullOrUndefined(title) && title.length>0)
+                        toPlayText += (Utils.correctTextForSpeech(title) + '. ');
+                    toPlayText += Utils.correctTextForSpeech(summary);
+                    toPlayText  += '. ';
+                }
+            );
+            this.setState({
+                playStatus: 'start'
+            });
+            this.speech.speak({
+                text: toPlayText,
+                queue: false
+            }).then(() => {
+                this.setState({
+                    playStatus: 'end'
+                });
+                //console.log('here');
+            }).catch(e => {
+                console.error("An error occurred :", e)
+            });
+            
+        }
+    }
+
+    async componentDidMount(){
+        await this.initSpeech();
+
+        if(isChrome)
+            this.timeInFn();
+    }
+
+    async componentWillUnmount(){
+        if(!isNullOrUndefined(this.speech)){
+            await this.speech.cancel();
+        }
     }
 
     BlockEvidence(evidence, index){
@@ -133,14 +271,38 @@ class TimelineComponent extends React.Component {
       this.renderTimeline(timelineBlock, index)
     );
       return (
-        <div style={{background:'lightblue'}}>
-            <VerticalTimeline> 
-                {timelineView}
-                <VerticalTimelineElement
-                    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-                />
-            </VerticalTimeline>
-       </div>
+          <div>
+              {!isIE && this.state.isSpeechAvailable?
+                <div className='timeline-block-list-sound'>
+                    {this.state.playStatus == 'end'?
+                        <a onClick={this.playExistingSelection} className="soundIcon">
+                            <PlayArrow />
+                        </a>
+                        :
+                        null
+                    }                                       
+
+                    {(this.state.playStatus == 'start' || this.state.playStatus == 'paused')?
+                        <a onClick={this.stopExistingSelection} className="soundIcon">
+                            <Stop />
+                        </a>
+                        :
+                        null
+                    }    
+                </div>
+                :
+                null
+            }
+            
+            <div style={{background:'lightblue'}}>
+                <VerticalTimeline> 
+                    {timelineView}
+                    <VerticalTimelineElement
+                        iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                    />
+                </VerticalTimeline>
+            </div>
+          </div>
       );
     }
   }
