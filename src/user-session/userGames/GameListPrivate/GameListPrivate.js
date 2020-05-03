b036bb5d1eb940fc1563832cc4beece0c27d919a
Shashank <shashank95.a@gmail.com>
2020-05-03 18:53:58 +0530

    added new game

--- src/user-session/userGames/GameListPrivate/GameListPrivate.js
+++ src/user-session/userGames/GameListPrivate/GameListPrivate.js
@@ -4,6 +4,8 @@ import { isNullOrUndefined } from 'util';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import * as Utils from '../../../common/utilSvc';
 import Loader from 'react-loader-spinner';
 import SingleGameListItemComponent from '../../../viso/gameList/singleGameElement/SingleGameElement';
 import './GameListPrivate.css';
@@ -19,7 +21,11 @@ class GameListPrivate extends React.Component {
           title: null,
           isLoading: true,
           currGameList: {},
-          addGameList: false
+          addGameList: false,
+          newGame: {
+              title: '',
+              id: ''
+          }
       }
 
       this.removeGame = this.removeGame.bind(this);
@@ -27,6 +33,9 @@ class GameListPrivate extends React.Component {
       this.writeToDb = this.writeToDb.bind(this);
       this.singleGameListItem = this.singleGameListItem.bind(this);
       this.renderGameListFull = this.renderGameListFull.bind(this);
+      this.confirmGame = this.confirmGame.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+      this.isValidGame = this.isValidGame.bind(this);
     }
 
     removeGame(gameId){
@@ -49,7 +58,8 @@ class GameListPrivate extends React.Component {
 
     async writeToDb(list, title){
         let currGameList = this.state.currGameList;
-        currGameList['list'] = list;
+        currGameList['games'] = list;
+        delete currGameList['list'];
         currGameList['title'] = title;
         await firebase.firestore().collection('publicGameList').doc(this.props.gameListId).set(currGameList);
     }
@@ -65,6 +75,29 @@ class GameListPrivate extends React.Component {
                 />
             </div>
         )
+    }
+
+    handleChange(event, type) {
+
+        var shouldUpdate = true;
+        if(type!="date" && type!="time"){
+            let newStr = event.target.value;
+            if(!Utils.shouldUpdateText(newStr, '\n\t')){
+                shouldUpdate=false;
+            }
+        }
+
+        if(shouldUpdate){
+            var game = this.state.newGame;
+            if(type=="title"){
+                game.title = event.target.value;
+                this.setState({newGame: game});
+            }
+            else if(type=="id"){
+                game.id = event.target.value;
+                this.setState({newGame: game});
+            }
+        }
     }
 
     componentDidMount(){
@@ -83,7 +116,6 @@ class GameListPrivate extends React.Component {
                         title: title,
                         currGameList: currGameList
                     });
-                    console.log(currGameList);
                 }
                 scope.setState({
                     isLoading: false
@@ -97,9 +129,101 @@ class GameListPrivate extends React.Component {
         }
     }
 
+    confirmGame(){
+        let newGame = this.state.newGame;
+        this.addGame(newGame);
+        this.setState({
+            addGameList: false,
+            newGame: {
+                title: '',
+                id: ''
+            }
+        })
+    }
+
+    isValidGame(){
+        if(isNullOrUndefined(this.state.newGame.id) || this.state.newGame.id.trim()=='')
+            return false;
+        if(isNullOrUndefined(this.state.newGame.title) || this.state.newGame.title.trim()=='')
+            return false;
+        return true;
+    }
+
+    returnNewGame(){
+        return (
+            <div>
+                <Grid xs={12}>
+                    <div className="newGameFormContainer">
+                        <form>
+                            <label>
+                                <TextField 
+                                    type="text"
+                                    variant="outlined"
+                                    multiline
+                                    label = "Game title"
+                                    value={this.state.newGame.title}
+                                    onChange={(e) => { this.handleChange(e,"title")}}
+                                    rowsMax="2"
+                                    rowsMin="1"
+                                    style={{
+                                        background: 'white',
+                                        marginTop:'6px',
+                                        marginBottom:'6px',
+                                        width:'95%'
+                                        }}/>
+                                <TextField 
+                                type="text"
+                                variant="outlined"
+                                multiline
+                                label = "Game if"
+                                value={this.state.newGame.id}
+                                onChange={(e) => { this.handleChange(e,"id")}}
+                                rowsMax="2"
+                                rows="1"
+                                style={{
+                                    background: 'white',
+                                    marginTop:'6px',
+                                    marginBottom:'6px',
+                                    width:'95%'
+                                    }}/>
+                            </label>
+                        </form>
+                        <div className="newGamePrivateOptionsContainer">
+                            {
+                                this.isValidGame()?
+                                <Button
+                                    className="addGameButton"
+                                    color="primary"
+                                    variant="contained"
+                                    onClick={() => { this.confirmGame() }}>
+                                        Add
+                                </Button>
+                                :
+                                null
+                            }
+                            <Button
+                                className="addGameButton"
+                                color="primary"
+                                variant="contained"
+                                onClick={() => { 
+                                    this.setState({
+                                        addGameList: false,
+                                        newGame: {
+                                            title: '',
+                                            id: ''
+                                        }})
+                                    }}>
+                                    Close
+                            </Button>
+                        </div>                       
+                    </div>
+                </Grid>
+            </div>
+        )
+    }
+
     renderGameListFull(){
         let list = this.state.list;
-
         let displayList = list.map(game => {
             return this.singleGameListItem(game);
         })
@@ -124,15 +248,15 @@ class GameListPrivate extends React.Component {
                                 Add new game
                         </Button>
                             : 
-                        <Button
-                            className="addGameButton"
-                            color="primary"
-                            variant="contained"
-                            onClick={() => { this.setState({addGameList: false})}}>
-                                Close
-                        </Button>    
+                        null 
                     }
                 </div>
+
+                {this.state.addGameList?
+                    this.returnNewGame()
+                    :
+                    null
+                }
 
                 {this.state.list.length > 0?
                     <div style={{marginBottom:'40px'}}>{displayList}</div>
