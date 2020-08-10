d0dff16958218ca40c960632ef4fd1e914d45979
Shashank <shashank95.a@gmail.com>
2020-08-11 05:06:58 +0530

    Offset top fixed

--- src/viso/dissectPicture/dissectPicture.js
+++ src/viso/dissectPicture/dissectPicture.js
@@ -10,9 +10,29 @@ class DissectPictureView extends React.Component {
 
     constructor(props){
         super(props);
+        //addBlock
 
         this.state ={
-            showLines: false
+            showLines: false,
+            lines: [
+                {
+                    x0:1/7,
+                    y0:321/652,
+                    x1:0.2,
+                    y1:0.1
+                },
+                {
+                    x0:443/570,
+                    y0:447/570,
+                    x1:0.8,
+                    y1:0.95
+                }
+            ],
+            pos:{
+                first:null,
+                second: null
+            },
+            addBlock: true
         }
 
         this.imgUrl = 'https://i.pinimg.com/564x/3d/22/ef/3d22ef2dc19d25469b0c4f75ce868118.jpg';
@@ -20,39 +40,65 @@ class DissectPictureView extends React.Component {
         this.renderLine = this.renderLine.bind(this);
         this.renderLines = this.renderLines.bind(this);
         this.mouseClick = this.mouseClick.bind(this);
-
+        this.displayPositions = this.displayPositions.bind(this);
     }
 
     mouseClick(event) {
-        let rect = document.getElementById("testing").getBoundingClientRect();
         let coord = {x:event.clientX, y:event.clientY};
-        console.log(rect);
-        console.log(coord);
+
+        coord.x = coord.x - document.getElementById("testing").getBoundingClientRect().x
+        + document.getElementById("testing").offsetLeft;
+        coord.y = coord.y - document.getElementById("testing").getBoundingClientRect().y
+        + document.getElementById("testing").offsetTop;
+        let pos = this.state.pos;
+        if(isNullOrUndefined(pos.first)){
+            pos.first = coord;
+        }
+        else if(!isNullOrUndefined(pos.first) && isNullOrUndefined(pos.second)){
+            pos.second = coord;
+        }
+        else{
+            //Function to return value here
+            pos.first = coord;
+            pos.second = null;
+        }
+        this.setState({
+            pos:pos
+        });
+    }
+
+    displayPositions(pos){
+
+        return(
+            <Fragment>
+                {!isNullOrUndefined(pos.first)?
+                    <Fragment>
+                        {this.renderCircle(pos.first.x,pos.first.y)}
+                    </Fragment>
+                    :
+                    null
+                }
+                {!isNullOrUndefined(pos.second)?
+                    <Fragment>
+                        {this.renderCircle(pos.second.x,pos.second.y)}
+                        {this.renderLine(pos.first.x,pos.first.y,pos.second.x,pos.second.y)}
+                    </Fragment>
+                    :
+                    null
+                }
+            </Fragment>
+        )
     }
 
     componentDidMount(){ 
-        this.elem = document.getElementById("testing");         
-        document.addEventListener("click", this.mouseClick);
+        document.getElementById("testing").addEventListener("click", this.mouseClick);
         this.setState({
             showLines: true
         })
     }
 
     renderLines(){
-        let lines = [
-            {
-                x0:1/7,
-                y0:321/652,
-                x1:0.2,
-                y1:0.1
-            },
-            {
-                x0:443/570,
-                y0:447/570,
-                x1:0.8,
-                y1:0.95
-            }
-        ];
+        let lines = this.state.lines;
 
         let lineRender = lines.map((line) => {
             let f0 = [
@@ -105,11 +151,18 @@ class DissectPictureView extends React.Component {
         return (
             <div style={{margin: '1em'}}>
                 <div id="testing" className="imageDissectContainer">
-                   <img className="imageToDissect" src={this.imgUrl} />    
-                         
-                    {this.state.showLines?
+                   <img className={"imageToDissect " + 
+                   (this.state.addBlock?"imageToDissect_addBlock ":null)} src={this.imgUrl} />        
+                    {this.state.showLines && !this.state.addBlock?
                         <Fragment>
                             {this.renderLines()}
+                        </Fragment>
+                        :
+                        null
+                    }
+                    {this.state.showLines && this.state.addBlock?
+                        <Fragment>
+                            {this.displayPositions(this.state.pos)}
                         </Fragment>
                         :
                         null
